66528165873f34a86a790e696f0c8cc52c317e66
zabdi908-glitch <zabdi908@gmail.com>
2026-06-30 10:33:47 +0100

    Update tailwind.config.js

--- cherrywood-inventory-main/tailwind.config.js
+++ cherrywood-inventory-main/tailwind.config.js
@@ -1,1 +1,10 @@
-module.exports = { content: ["./templates/**/*.html"], theme: { extend: {} }, plugins: [], }
+module.exports = {
+  content: [
+    "./cherrywood-inventory-main/templates/**/*.html",
+    "./cherrywood-inventory-main/static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
